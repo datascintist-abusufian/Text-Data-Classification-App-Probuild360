cce78f11d19c77968fbdcaca26511fff3df63c8e
Data Analyst <abusufian.tex.cu@gmail.com>
2024-01-30 22:15:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
 
 # Display the GIF image using st.image
-st.image("activation.gif", width=600)
+st.image("activation.gif", use_column_width=True)
 
 # Title and description
 st.title("Text Data Classification App-Probuild360")
