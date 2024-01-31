7b4b12b7694ce3c9384bd6cfd84a5d9cff9581cc
Data Analyst <abusufian.tex.cu@gmail.com>
2024-01-31 00:39:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.image("activation.gif", use_column_width=True)
 # Title and description
 st.title("Text Data Classification App-Probuild360")
 st.write("This app demonstrates text classification into different classes using Streamlit.")
-st.write (" Author Md Abu Sufian)
+st.write ("Author Md Abu Sufian")
 # Sidebar for file upload
 
 st.sidebar.header("Upload Your Data")
