74ddc9bc982f4debdfa9c0c5b701a9e4dc8fc64d
Data Analyst <abusufian.tex.cu@gmail.com>
2024-02-06 21:28:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ if st.session_state['load_example']:
         st.dataframe(df_example.head())
             
 # Load the dataset
-df = load_data_from_github()
+df, message = load_data_from_github()
 
 # Check if the dataset is loaded
 if df is not None and not df.empty:
@@ -127,4 +127,4 @@ if text_input:
             st.write("Prediction Result:")
             st.success(f"The predicted class for the entered text is: {prediction[0]}")
 else:
-    st.error("Failed to load data. Please check the dataset url or format")
+    st.error(message)
