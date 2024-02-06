882fe8eb8c9623d3c52e6d4e115eea023dae766b
Data Analyst <abusufian.tex.cu@gmail.com>
2024-02-06 21:45:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -127,4 +127,5 @@ if text_input:
             st.write("Prediction Result:")
             st.success(f"The predicted class for the entered text is: {prediction[0]}")
 else:
-    st.error("Failed to load data. Please check the dataset url or format. Error: " + error_message")
+    st.error("Failed to load data. Please check the dataset url or format. Error: " + error_message)
+
